51de87a48e499d5936c8bcca2a5dcc234d9e5604
BeerFairy <moodholly78@gmail.com>
2021-05-01 16:40:06 +0600

    refactored views, added generics classes

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,3 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import generics
 from django.db import models
 from django.db.models import Q
@@ -9,46 +7,40 @@ from . import serializers as sr
 from movies.services import get_client_ip
 
 
-class MovieListView(APIView):
+class MovieListView(generics.ListAPIView):
 	'''output movie list'''
-	def get(self, request):
+
+	serializer_class = sr.MovieListSerializer
+
+	def get_queryset(self):
 		movies = Movie.objects.filter(draft=False).annotate(
 			rating_user=models.Count('ratings', 
-				filter=models.Q(ratings__ip=get_client_ip(request)))
+				filter=models.Q(ratings__ip=get_client_ip(self.request)))
 			).annotate(
 				middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
 			)
-		serializer = sr.MovieListSerializer(movies, many=True)
-		return Response(serializer.data)
+		return movies
 
 
-class MovieDetailView(APIView):
+class MovieDetailView(generics.RetrieveAPIView):
 	'''output movie detail'''
-	def get(self, request, pk):
-		movie = Movie.objects.get(id=pk, draft=False)
-		serializer = sr.MovieDetailSerializer(movie)
-		return Response(serializer.data)
+	queryset = Movie.objects.filter(draft=False)
+	serializer_class = sr.MovieDetailSerializer
 
 
-class ReviewCreateView(APIView):
+
+class ReviewCreateView(generics.CreateAPIView):
 	'''adding reviews in film'''
-	def post(self, request):
-		review = sr.ReviewCreateSerializer(data=request.data)
-		if review.is_valid():
-			review.save()
-		return Response(status=201)
+	serializer_class = sr.ReviewCreateSerializer
 
 
-class AddStarRatingView(APIView):
+class AddStarRatingView(generics.CreateAPIView):
 	'''adding film rating'''
+	serializer_class = sr.CreateRatingSerializer
 
-	def post(self, request):
-		serializer = sr.CreateRatingSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save(ip=get_client_ip(request))
-			return Response(status=201)
-		else:
-			return Response(status=400)
+	def perform_create(self, serializer):
+		serializer.save(ip=get_client_ip(self.request))
+
 
 
 class ActorListView(generics.ListAPIView):
