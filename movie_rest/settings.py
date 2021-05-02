2375a7ef407748d14e8701fd20c1c621a0fb59a9
BeerFairy <moodholly78@gmail.com>
2021-05-02 19:15:23 +0600

    added vk auth

--- movie_rest/settings.py
+++ movie_rest/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django_filters',
     'djoser',
     'drf_yasg',
+    'oauth2_provider',
+    'social_django',
+    'rest_framework_social_oauth2',
 
     'movies.apps.MoviesConfig',
 ]
@@ -211,10 +214,23 @@ CKEDITOR_CONFIGS = {
 }
 
 
+SOCAIL_AUTH_VK_OAUTH2_KEY = '7834815'
+SOCAIL_AUTH_VK_OAUTH2_SECRET = 'ckNQAXRVNey344jWjQqE'
+
+
+AUTHETICATION_BACKENDS = (
+    'social.core.backends.vk.VKOAuth2',
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
